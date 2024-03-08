4e0f2066ee2e79fbbce1a030c614ac119177bd94
James Kabbes <james.kabbes@gmail.com>
2024-03-08 12:10:53 -0600

    changing test case

--- src/kabbes_matchup_scheduler/__main__.py
+++ src/kabbes_matchup_scheduler/__main__.py
@@ -1,7 +1,9 @@
 from kabbes_matchup_scheduler.scheduler import Scheduler
 scheduler = Scheduler(**{
     'n_teams': 8,
-    'n_rounds': 6,
+    'n_rounds': 7,
     'teams_per_matchup': 2,
     'matchups_per_round': None,
 })
+
+scheduler.run()
