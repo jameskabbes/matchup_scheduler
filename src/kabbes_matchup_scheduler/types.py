c8d8037db7c82ba0f79e813f955488b62d26dc16
James Kabbes <james.kabbes@gmail.com>
2024-03-08 12:07:51 -0600

    moving to types

--- src/kabbes_matchup_scheduler/types.py
+++ src/kabbes_matchup_scheduler/types.py
@@ -1,4 +1,27 @@
+from typing import TypedDict
+
 type IDType = int
+type TeamID = IDType
+
+type LocaleIndex = int
+
 type MatchupType = list[IDType]
 type RoundType = list[MatchupType]
 type ScheduleType = list[RoundType]
+
+
+ConstraintConfig = TypedDict('ConstraintConfig', {
+    'min': int,
+    'max': int
+})
+
+OpponentCountConfig = TypedDict('OpponentCountConfig', {
+    'count': ConstraintConfig,
+    'locale': ConstraintConfig
+})
+
+
+class ConstraintsConfig(TypedDict):
+    n_matchups: ConstraintConfig
+    opponent_count: OpponentCountConfig
+    locale_count: ConstraintConfig
