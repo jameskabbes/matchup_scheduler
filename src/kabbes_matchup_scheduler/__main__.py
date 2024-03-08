8a59d0db5d52f3341c954d406b50a902cfb06891
James Kabbes <james.kabbes@gmail.com>
2024-03-08 12:54:34 -0600

    changing main params

--- src/kabbes_matchup_scheduler/__main__.py
+++ src/kabbes_matchup_scheduler/__main__.py
@@ -1,7 +1,7 @@
 from kabbes_matchup_scheduler.scheduler import Scheduler
 scheduler = Scheduler(**{
-    'n_teams': 8,
-    'n_rounds': 7,
+    'n_teams': 4,
+    'n_rounds': 3,
     'teams_per_matchup': 2,
     'matchups_per_round': None,
 })
